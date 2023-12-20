dcf1dae33a62f5f83b1b3d21f5e62b54b494f04b
Kevin-Locatelli <111590089+Kevin-Locatelli@users.noreply.github.com>
2023-12-20 14:08:05 +0100

    add endpoint /users

--- src/index.js
+++ src/index.js
@@ -1,8 +1,70 @@
 const express = require('express');
+const Crypto = require('crypto');
+const fs = require('fs');
+const jwt = require('jsonwebtoken');
+const { Sequelize, DataTypes, Model } = require('sequelize');
+const bodyParser = require("body-parser");
+
 const app = express();
+app.use(bodyParser.json());
+
 const port = 3000;
 
-import './login.js';
+// Connexion à la base de données
+const sequelize = new Sequelize('wive_teleportme', 'wive', 'WiveTeleportMe13', {
+    host: 'mysql-wive.alwaysdata.net',
+    dialect: 'mysql',
+});
+
+class Users extends Model {}
+
+Users.init({
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    role: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    }
+}, {
+    sequelize,
+    modelName: 'wive_teleportme',
+    tableName: 'users'
+});
+
+sequelize.sync()
+    .then(() => {
+        console.log('Database and tables synced');
+    })
+    .catch((err) => {
+        console.error('Error syncing database:', err);
+    });
+
+app.get('/users', (req, res) => {
+    const auth = req.header('Authorization');
+
+    const isBasicAuth = auth && auth.startsWith('Basic ');
+    if (!isBasicAuth) {
+        res.status(401).send('Unauthorized');
+        return;
+    }
+
+    res.json(Users.findAll());
+});
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}!`);
