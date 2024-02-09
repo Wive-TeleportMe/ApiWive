e35f8b639d35679c7614936ef840d1ca226f0a56
Kevin-Locatelli <111590089+Kevin-Locatelli@users.noreply.github.com>
2024-02-09 10:43:05 +0100

    Change port

--- src/camera.js
+++ src/camera.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const cors = require('cors')
-const port = 3000;
+const port = 3001;
 const { Sequelize, DataTypes, Model } = require('sequelize');
 const {readFileSync} = require('fs');
 
