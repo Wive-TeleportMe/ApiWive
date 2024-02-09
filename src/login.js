c083d5e1d40d08e66a609dc271680c0fb56430b8
Kevin-Locatelli <111590089+Kevin-Locatelli@users.noreply.github.com>
2024-02-09 10:42:02 +0100

    fix merge error

--- src/login.js
+++ src/login.js
@@ -1,4 +1,60 @@
-/*app.get('/renew', (req, res) => {
+const express = require('express');
+const Crypto = require('crypto');
+const fs = require('fs');
+const jwt = require('jsonwebtoken');
+const { Sequelize, DataTypes, Model } = require('sequelize');
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.json());
+
+const port = 3000;
+
+// Connexion à la base de données
+const sequelize = new Sequelize('wive_teleportme', 'wive', 'WiveTeleportMe13', {
+    host: 'mysql-wive.alwaysdata.net',
+    dialect: 'mysql',
+});
+
+class Users extends Model {}
+
+Users.init({
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    role: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    }
+}, {
+    sequelize,
+    modelName: 'wive_teleportme',
+    tableName: 'users'
+});
+
+sequelize.sync()
+    .then(() => {
+        console.log('Database and tables synced');
+    })
+    .catch((err) => {
+        console.error('Error syncing database:', err);
+    });
+
+app.get('/renew', (req, res) => {
     const auth = req.header('Authorization');
 
     const isBasicAuth = auth && auth.startsWith('Basic ');
@@ -23,9 +79,9 @@
 
     res.status(200).send('Renew Successful');
 
-});*/
+});
 
-/*app.get('/editor', (req, res) => {
+app.get('/editor', (req, res) => {
     const auth = req.header('Authorization');
 
     const isBasicAuth = auth && auth.startsWith('Basic ');
@@ -38,19 +94,19 @@
 
     let valideUser = null;
 
-    for (const user of users) {
+    for (const user of Users) {
         if (user.local_user === decodedValue.local_user || decodedValue.local_user === 'admin') {
             valideUser = user;
         }
     }
 
     if (valideUser != null) {
-        if (req.query.user != undefined){
-            for (const user of users) {
+        if (req.query.user !== undefined){
+            for (const user of Users) {
                 if (user.local_user === decodedValue.local_user) {
                     user.local_user = req.query.user;
-                    users.forEach(function (item, index) {
-                        fs.writeFile('users.json', JSON.stringify(users), function (err) {
+                    Users.forEach(function (item, index) {
+                        fs.writeFile('users.json', JSON.stringify(Users), function (err) {
                             if (err) return console.log(err);
                         });
                     });
@@ -58,12 +114,12 @@
             }
         }
 
-        if (req.query.password != undefined) {
-            for (const user of users) {
+        if (req.query.password !== undefined) {
+            for (const user of Users) {
                 if (user.local_user === decodedValue.local_user) {
                     user.local_password = Crypto.createHash('SHA256').update(req.query.password).digest('hex');
-                    users.forEach(function (item, index) {
-                        fs.writeFile('users.json', JSON.stringify(users), function (err) {
+                    Users.forEach(function (item, index) {
+                        fs.writeFile('users.json', JSON.stringify(Users), function (err) {
                             if (err) return console.log(err);
                         });
                     });
@@ -75,4 +131,4 @@
     } else {
         res.status(401).send('Unauthorized');
     }
-});*/
+});
