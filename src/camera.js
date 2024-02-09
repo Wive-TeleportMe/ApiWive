a8b1357c06cc38730b03f87c5a32fc2d957ff9f3
VeryOldpApY <VeryOldpApY@pm.me>
2024-02-09 10:36:16 +0100

    fix port

--- src/camera.js
+++ src/camera.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 3000;
+const port = 3001;
 const { Sequelize, DataTypes, Model } = require('sequelize');
 const {readFileSync} = require('fs');
 
